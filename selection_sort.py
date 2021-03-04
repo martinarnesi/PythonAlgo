c998d2b143269c3d8a55bc65d9f9f72863a8df58
Martin Arnesi <martinarnesi@gmail.com>
2021-03-04 16:46:31 -0300

    Adding Selection Sort

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+if __name__ == '__main__':
+    my_list = [5, 3, 6, 2, 10]
+    print(selectionSort(my_list))
